22e0552d3a0b9108210eae8202ddd6fdcdfd8c5e
Ezequiel <ezequiel.rm021@gmail.com>
2025-10-28 10:02:12 -0600

    solved merge conflicts

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+<<<<<<< HEAD
 swww-daemon -f xrgb &
 BACK_PID=$!
 wait $BACK_PID
 swww img /home/ramos/Pictures/fontaine.jpeg
+=======
+
+wal -i $1 -e;
+hyprctl hyprpaper reload ",$1"
+>>>>>>> 8a1280a (updated configuration)
